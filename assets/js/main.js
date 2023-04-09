cee5c79edd6f82d09bf42ced48932fa2a51f843e
Aleksei <scrnr@outlook.com>
2023-04-10 02:11:15 +0300

    Creating a game template

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const gameField = document.getElementById('game');
+const gameStartBtn = document.getElementById('btn-start');
+const newWordBtn = document.getElementById('new-word-btn');
+
+gameStartBtn.onclick = () => {
+    // start
+}
+
+newWordBtn.onclick = () => {
+    // restart
+}
